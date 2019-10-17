8a6cda41bad98b010e9fc88412b85719a1dfc1b2
frank okafor <frankokafor22@yahoo.com>
2019-10-17 12:15:51 +0100

    Authorization filter config

--- Photo_Application/src/main/java/com/frankokafor/rest/security/WebSecurity.java
+++ Photo_Application/src/main/java/com/frankokafor/rest/security/WebSecurity.java
@@ -5,6 +5,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.frankokafor.rest.services.UserService;
@@ -30,6 +31,18 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 		http.csrf().disable().authorizeRequests()
 			.antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL).permitAll()
 			.anyRequest().authenticated().and()
-			.addFilter(new AuthenticationFilter(authenticationManager()));
+			.addFilter(getAuthentication())
+			.addFilter(new AuthorizationFilter(authenticationManager()))
+			.sessionManagement()
+			.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	/*because most times 
+	 * 		
+	 */
+	}
+	
+	public AuthenticationFilter getAuthentication() throws Exception {
+		final AuthenticationFilter filter = new AuthenticationFilter(authenticationManager());
+		filter.setFilterProcessesUrl("/users/login");
+		return filter;
 	}
 }
